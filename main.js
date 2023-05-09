cc97ac0ecd5659de19d3cbeb970267850bb4d156
matthewmaloneydev@gmail.com <matthew@Ruobings-MacBook-Pro.local>
2023-05-08 20:30:07 -0500

    added repos object and continued work on reposCardsOnDom function

--- main.js
+++ main.js
@@ -4,8 +4,24 @@ const renderToDom = (divId, htmltoRender) => {
 };
 
 const reposCardsOnDom = () => {
-  let domString = "";
-  domString += `
-  
-  `;
+  let domString =
+    "<div><input type='text' id='findRepo' placeholder='Find a repository...'></div>";
+
+  repos.forEach((repo) => {
+    domString += ``;
+  });
 };
+
+const repos = [
+  {
+    name: "serious-coding-project",
+    description: "a very super serious coding project",
+    tags: ["react", "javascript", "superCode", "vim"],
+    language: "JavaScript",
+    stars: 500,
+    branches: 70,
+    issues: 0,
+    updatedDate: "2023-05-08",
+    starred: false,
+  },
+];
